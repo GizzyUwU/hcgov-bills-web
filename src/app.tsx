06ff2058c331a20ae3f3554149362f5539477f9a
gizzy <me@gizzy.pro>
2025-12-19 09:45:01 +0000

    feat: meta tags

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { MetaProvider, Title } from "@solidjs/meta";
+import { MetaProvider, Title, Meta } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
@@ -13,6 +13,19 @@ export default function App() {
       root={props => (
         <MetaProvider>
           <Title>Hack Club Parliament - Bills and Propositions</Title>
+          <Meta name="description" content="View of bills and propositions from Hack Club Parliament, listing the number that has been passed, and those that have been rejected or nullled or ones that are." />
+          <Meta itemprop="name" content="Hack Club Parliament - Bills and Propositions" />
+          <Meta itemprop="description" content="View of bills and propositions from Hack Club Parliament, listing the number that has been passed, and those that have been rejected or nullled or ones that are." />
+          <Meta itemprop="image" content="https://gov.avcap.xyz/meta.svg" />
+          <Meta property="og:url" content="https://gov.avcap.xyz/" />
+          <Meta property="og:type" content="website" />
+          <Meta property="og:title" content="Hack Club Parliament - Bills and Propositions" />
+          <Meta property="og:description" content="View of bills and propositions from Hack Club Parliament, listing the number that has been passed, and those that have been rejected or nullled or ones that are." />
+          <Meta property="og:image" content="https://gov.avcap.xyz/meta.svg" />
+          <Meta name="twitter:card" content="summary_large_image" />
+          <Meta name="twitter:title" content="Hack Club Parliament - Bills and Propositions" />
+          <Meta name="twitter:description" content="View of bills and propositions from Hack Club Parliament, listing the number that has been passed, and those that have been rejected or nullled or ones that are." />
+          <Meta name="twitter:image" content="https://gov.avcap.xyz/meta.svg" />
           <header class="govuk-header" data-module="govuk-header">
             <div class="govuk-header__container govuk-width-container">
               <div class="govuk-header__logo">
